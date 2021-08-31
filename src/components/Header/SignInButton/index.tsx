62694360134bc36f91cb5844aec5bd13bd949ad5
NoronhaGCP <inc.691@gmail.com>
2021-08-31 12:13:51 -0300

    Add github authentication

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -2,28 +2,37 @@ import React from 'react';
 import { useState } from 'react';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 const getUnloggedJSX = () => (
-  <button className={styles.signInButton} type="button">
+  <button 
+    className={styles.signInButton} 
+    type="button"
+    onClick={() => signIn('github')}
+  >
     <FaGithub color="#eba417" />
     Sign in with Github
   </button>
 );
 
-const getLoggedJSX = () => (
-  <button className={styles.signInButton} type="button">
+const getLoggedJSX = (username: string) => (
+  <button 
+    className={styles.signInButton} 
+    type="button"
+    onClick={() => signOut()}
+  >
     <FaGithub color="#04d361" />
-    Gabriel Noronha
+    { username }
     <FiX color="#737380" className={styles.closeIcon} />
   </button>
 );
 
 export function SignInButton() {
-  const [ isUsedLoggedIn, setIsUsedLoggedIn ] = useState(false);
+  const [session] = useSession();
 
-  return isUsedLoggedIn 
-    ? getLoggedJSX()
+  return session
+    ? getLoggedJSX(session.user.name)
     : getUnloggedJSX();
 }
